f5c87463d77d9bb13cce3f5d88fd7fb95c76fd05
work <work@death>
2016-12-21 21:03:01 +0800

    change some definition

--- include/shared_memory.h
+++ include/shared_memory.h
@@ -2,9 +2,10 @@
 #define SHARED_MEMORY_H
 
 const int KEY = 0x18;
-const int MAX_FILE_NAME = 10;
+const int MAX_NAME_LENGTH = 10;
 const int MAX_FILE_SIZE = 1000;
 const int MAX_FILE_NUMBER = 10;
+const int MAX_BLOCK_NUMBER = 4;
 
 enum CommantType
 {
@@ -19,6 +20,7 @@ struct Command_t
 {
 	uint8_t type;
 	pid_t pid;
+	bool hasError;
 	int request;
 };
 
@@ -30,6 +32,7 @@ struct Information_t
 struct File_t
 {
 	short length;
+	char name[MAX_NAME_LENGTH];
 	char data[ MAX_FILE_SIZE ];
 };
 
