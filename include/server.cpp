7b0c969b81a91b418917825f1d76e7fd38e1fe64
work <work@death>
2016-12-22 18:53:23 +0800

    complete class Server

--- include/server.cpp
+++ include/server.cpp
@@ -1,9 +1,39 @@
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <string.h>
 #include "server.h"
 
-int Server::findBlock( Pid_t owner )
+Server::Server( void )
+{
+	semid = semget( SEM_KEY, 1,  IPC_CREAT|0777 );
+	size_t size = 0;
+	size += sizeof(Command_t);
+	size += sizeof(Information_t);
+	size += sizeof(Block_t)*MAX_BLOCK_NUMBER;
+
+	shmid = shmget( SHM_KEY, size, IPC_CREAT|0777 );
+	address = shmat( shmid, NULL, 0 );
+
+	char* pMemory = (char*)address;
+	
+	pMemory += 0;
+
+	command = (Command_t*)pMemory;
+	pMemory += sizeof(Command_t);
+
+	information = (Information_t*)pMemory;
+	pMemory += sizeof(Information_t);
+
+	for( int i=0; i<MAX_BLOCK_NUMBER; i++ ) {
+		blocks[i] = (Block_t*)pMemory;
+		pMemory += sizeof(Block_t);
+	}
+}
+
+int Server::findBlock( pid_t owner )
 {
 	int tmp = -1;
-	for( int i=0; i<MAX_BLOCKS_NUMBER; i++ ) {
+	for( int i=0; i<MAX_BLOCK_NUMBER; i++ ) {
 		if( blocks[i]->owner == owner ) {
 			tmp = i;
 			break;
@@ -14,28 +44,24 @@ int Server::findBlock( Pid_t owner )
 
 void Server::applyBlock( void )
 {
-	if( numberOfBlocks > MAX_BLOCKS_NUMBER ) {
-		command->hasError = true;
+	int ptr = -1;
+	if( (ptr = findBlock(0)) >= 0 ) {
+		blocks[ptr]->owner = command->pid;
+		command->request = (char*)blocks[ptr] - (char*)address;
+		command->type = 0;
 	}
 	else {
-		int ptr = -1;
-		if( (ptr = findBlock(0)) >= 0 ) {
-			blocks[ptr]->owner = command->pid;
-			numberOfBlocks++;
-			command->request = (char*)blocks[ptr] - (char*)address;
-		}
-		else {
-			command->hasError = true;
-		}
+		command->hasError = true;
 	}
 }
 
 void Server::releaseBlock( void )
 {
 	int ptr = -1;
-	if( (ptr = findBlock(command->owner) ) >= 0 ) {
-		blocks[i]->owner = 0;
+	if( (ptr = findBlock(command->pid) ) >= 0 ) {
+		blocks[ptr]->owner = 0;
 		command->request = 1;
+		command->type = 0;
 	}
 	else {
 		command->hasError = true;
@@ -45,23 +71,78 @@ void Server::releaseBlock( void )
 void Server::showDirectory( void )
 {
 	int pBlock = findBlock( command->pid );
-	if( ptr < 0 ) {
+	if( pBlock < 0 ) {
 		command->hasError = true;
 		return ;
 	}
 
 	char* j = (blocks[pBlock]->file).data;
 	for( int i=0; i<directory.numberOfFiles; i++ ) {
-		char* pName = nameOfFile[i];
+		char* pName = (directory.files[i]).name;
 
-		while( j++ = pName++ );
-		j++ = '\n';
+		while( (*j++ = *pName++) != '\0' );
+		*j++ = '\n';
 	}
-	j = '\0';
-	blocks[pBlock]->file).length = short( j - (blocks[pBlock]->file).data );
-
+	*j = '\0';
 	command->request = 1;
+	command->type = 0;
 }
 
+void Server::putFile( void )
+{
+	if( directory.numberOfFiles >= MAX_FILE_NUMBER ) {
+		command->hasError = true;
+	}
+	else {
+		int pBlock = findBlock( command->pid );
+		if( pBlock >= 0 ) {
+			directory.addFile( blocks[pBlock]->file );
+		}
+		else {
+			command->hasError = true;
+		}
+	}
+}
 
+void Server::getFile( void )
+{
+	int pBlock = findBlock( command->pid );
+	if( pBlock >= 0 ) {
+		int i = 0;
+		for( i=0; i<directory.numberOfFiles; i++ ) {
+			if( !strcmp( (blocks[pBlock]->file).name, (directory.files[i]).name ) ) {
+			   copyFile( (blocks[pBlock]->file).data, (directory.files[i]).data );
+			   break;
+			}
+		}
 
+		if( i == directory.numberOfFiles ) {
+			command->hasError = true;
+		}
+		else {
+			command->request = 1;
+		}
+	}
+	else {
+		command->hasError = true;
+	}
+}
+
+void Server::copyFile( char* destination, char* source )
+{
+	while( (*destination++ = *source++) != '\0' ) ;
+}
+
+bool Server::hasNewCommand( void ) 
+{
+	struct sembuf sem;
+	memset( &sem, 0, sizeof(sem) );
+	semop( semid, &sem, 1 );
+	while( command->type != 0 ) ;
+	return true;
+}
+
+int Server::getCommandType( void )
+{
+	return command->type;
+}
--- include/server.h
+++ include/server.h
@@ -1,17 +1,22 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <server_def.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include "server_def.h"
 
 class Server
 {
 public:
-	Server();
+	Server( void );
 	void applyBlock( void );
 	void releaseBlock( void );
 	void getFile( void );
 	void putFile( void );
-	void show( void );
+	void showDirectory( void );
+	bool hasNewCommand( void );
+	int getCommandType( void );
 
 private:
 	int shmid;
@@ -20,9 +25,11 @@ private:
 	struct Directory_t directory;
 
 	struct Command_t* command;
-	struct Information* information;
-	struct Block_t* blocks[MAX_BLOCKS_NUMBER];
-	int numberOfBlocks;
+	struct Information_t* information;
+	struct Block_t* blocks[MAX_BLOCK_NUMBER];
+
+	int findBlock( pid_t owner );
+	void copyFile( char* destination, char* source );
 };
 
 #endif
