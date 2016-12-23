f822fc337ee32a5c12750bbdfcd3a87c1cf608b6
work <work@death>
2016-12-23 16:45:28 +0800

    complete class Client

--- include/client.cpp
+++ include/client.cpp
@@ -1,17 +1,33 @@
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <string.h>
 #include "client.h"
 
-void Client::setCommand( uint8_t commandType )
+#include <stdio.h>
+
+struct sembuf Client::semP = { 0, -1, 0 };
+struct sembuf Client::semV = { 0, 1, 0 };
+
+Client::Client( void ) 
 {
-	semop( semid, &semP, 1 );
-	command->pid = getpid();
-	command->type = commandType;
+	semid = semget( SEM_KEY, 1,  IPC_CREAT|0777 );
+	shmid = shmget( SHM_KEY, 0, IPC_CREAT|0777 );
+	if( semid < 0 ) {
+		printf("SEM\n");
+	}
+	if( shmid < 0 ) {
+		printf("SHM\n");
+	}
+	address = shmat( shmid, NULL, 0 );
+
+	command = (Command_t*)address;
 }
 
-void Client::resetCommand( void )
+Client::~Client( void )
 {
-	memset( command, 0, sizeof(command) );
-	semop( semid, &semV, 1 );
+	shmdt( address );
 }
+
 
 void Client::applyBlock( void )
 {
@@ -24,9 +40,106 @@ void Client::applyBlock( void )
 void Client::releaseBlock( void )
 {
 	setCommand( RELEASE );
-	waitCommand();
+	waitNewCommand();
 
 	resetCommand();
 }
 
+
+void Client::putFile( void )
+{
+	setCommand( PUT );
+	waitNewCommand();
+	resetCommand();
+}
+
+void Client::getFile( void )
+{
+	setCommand( GET );
+	waitNewCommand();
+	resetCommand();
+}
+
+
+void Client::showDirectory( void )
+{
+	setCommand( SHOW );
+	waitNewCommand();
+	resetCommand();
+}
+
+
+uint8_t Client::getCommandType( void )
+{
+	return command->type;
+}
+
+bool Client::waitNewCommand( void )
+{
+	while( !(command->hasError)  && !(command->request) )
+	{
+	}
+	return true;
+}
+
+
+void Client::setCommand( uint8_t commandType )
+{
+	semop( semid, &semP, 1 );
+	command->pid = getpid();
+	command->type = commandType;
+}
+
+void Client::resetCommand( void )
+{
+	memset( command, 0, sizeof(Command_t) );
+	semop( semid, &semV, 1 );
+}
+
+
+void Client::setFileName( char* name )
+{
+	if( this->file != NULL ) {
+		strcpy( (this->file)->name, name );
+	}
+}
+
+void Client::getFileName( char* name )
+{
+	if( this->file != NULL ) {
+		strcpy( name, (this->file)->name );
+	}
+}
+
+
+void Client::setFileData( char* data )
+{
+	if( this->file != NULL ) {
+		strcpy( (this->file)->data, data );
+	}
+}
+
+void Client::getFileData( char* data )
+{
+	if( this->file != NULL ) {
+		strcpy( data, (this->file)->data );
+	}
+}
+
+
+void Client::setFile( File_t* file )
+{
+	if( this->file != NULL ) {
+		setFileName( file->name );
+		setFileData( file->data );
+	}
+}
+
+void Client::getFile( File_t* file )
+{
+	if( this->file != NULL ) {
+		getFileName( file->name );
+		getFileData( file->data );
+	}
+}
 
--- include/client.h
+++ include/client.h
@@ -1,14 +1,42 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdint.h>
+#include "shared_memory.h"
+
 class Client
 {
 public:
-	Clinet();
+	static struct sembuf semP;
+	static struct sembuf semV;
+
+	Client();
 	~Client( void );
+
 	void applyBlock( void );
 	void releaseBlock( void );
+
+	void putFile( void );
+	void getFile( void );
+
+	void showDirectory( void );
+
+	uint8_t getCommandType( void );
+	bool waitNewCommand( void );
+
 	void setCommand( uint8_t commandType );
+	void resetCommand( void );
+
+	void setFileName( char* name );
+	void getFileName( char* name );
+
+	void setFileData( char* data );
+	void getFileData( char* data );
+
+	void setFile( File_t* file );
+	void getFile( File_t* file );
 
 private:
 	int shmid;
@@ -17,11 +45,6 @@ private:
 
 	struct File_t* file;
 	struct Command_t* command;
-
-	const struct sembuf semP = { 0,-1,0 };
-	const struct sembuf semV = { 0,1,0 };
-
-
 };
 
 #endif
